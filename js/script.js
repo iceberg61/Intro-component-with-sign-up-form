a1245764bf1b457b1f214eef9d836226e5690b74
Godstime <98828211+iceberg61@users.noreply.github.com>
2023-05-03 22:39:22 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,64 @@
-let form = document.getElementById('form'),
-    firstName = document.getElementById('First-Name'),
-    lastName = document.getElementById('Last-Name'),
+const form = document.getElementById('form'),
+    firstName = document.getElementById('firstname'),
+    lastName = document.getElementById('lastname'),
     email = document.getElementById('email'),
-    // iconError = document.getElementById('icon-error'),
-    password = document.getElementById('password'),
-    error = document.getElementById('submit'),
-    patterns = /^[^ ]+@[^ ]*\.[a-z]{3,4}$/;
-
-email.addEventListener('input', validateEmail) 
-function validateEmail() {
-    if (!email.value.match(patterns)) {
-        const emailError = document.getElementById('email-error');
-        emailError.innerHTML = "Looks like this is not an email";
-        iconError.src = '../images/icon-error.svg'
-        email.classList.add('error');
-    }
-}
+    password = document.getElementById('password');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    checkInputs();
+})
 
-    if (firstName.value.trim() === '' || firstName.value === null) {
-        const firstNameText = document.getElementById('first-name-error');
-        firstNameText.innerHTML = "First name cannot be empty";
-        // iconError.src = '../images/icon-error.svg'
+function checkInputs() {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (firstNameValue === '') {
+        setErrorFor(firstName, 'First name cannot be empty')
+    } else {
+        setSuccessFor(firstName)
+    };
+
+    if (lastNameValue === '') {
+        setErrorFor(lastName, 'First name cannot be empty')
+    } else {
+        setSuccessFor(lastName)
+    };
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be empty')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Looks like this is not an email')
+    }
+     else {
+        setSuccessFor(email)
     }
 
-    if (lastName.value.trim() === '' || lastName.value === null) {
-        const lastNameText = document.getElementById('last-name-error');
-        lastNameText.innerHTML = "Last name cannot be empty";
+    if (passwordValue === '') {
+        setErrorFor(password, 'password cannot be empty');
+    } else {
+        setSuccessFor(password)
     }
+};
 
-    if(password.value.length <= 6) {
-        const passwordError = document.getElementById('password-error');
-        passwordError.innerHTML = "password must be more than 6 characters";
-    }
-});
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const span = formControl.querySelector('span');
+    
+    // add error message inside span
+    span.innerText = message;
+
+    // add error class
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
